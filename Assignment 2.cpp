da7b8a58cd165cad085c9046038a9effc046efa4
Sharvin1106 <sharvin.kogilavanan@gmail.com>
2021-06-19 02:04:56 +0800

    First Part of the Menu

--- Assignment 2.cpp
+++ Assignment 2.cpp
@@ -4,35 +4,83 @@
 using namespace std;
 void initializeGraph(Graph&);
 void initializeWeights(Graph&, int);
-
+void MainMenu();
+bool checkStrong(Graph&, int);
 int main()
 {
+    //Five vertices
     int vertices = 5;
+
+    //Declaring main graph and graph with all weights
     Graph g(vertices);
     Graph allWeights(vertices);
 
+    //Initializing
     initializeGraph(g);
     initializeWeights(allWeights, vertices);
     
-    g.toString();
+    int choice = 0;
     
 
-    /*while (!g.isSC()) {
-        cout << "This is not strongly connected graph" << endl;
-        generateRandomEdge(vertices, g, allWeights);
-    }
+    do {
+        MainMenu();
+        cin >> choice;
+        //Validation for choice
+        while (choice < 1 || choice > 7) {
+            cout << "Invalid input." << endl;
+            cout << "Re-enter your choice." << endl;
+            cin >> choice;
+        }
 
-    cout << "Graph strongly connected graph" << endl << endl;
-    g.toString();*/
-
-
-    while (!g.DFS()) {
-       cout << "Graph doesn't contain cycle" << endl;
-       
-       g.generateRandomEdge(allWeights);
-    }
-    cout << "Graph is cyclic" << endl;
-    g.toString();
+        if (choice != 8) {
+            if (choice == 1) {
+                g.toString();
+                cin.get();
+                cin.get();
+            }
+            else if (choice == 2) {
+                while (!checkStrong(g, vertices)) {
+                    char selection;
+                    cout << "The graph is not strong connected " << endl;
+                    cout << "Would you like to make it strong?" << endl;
+                    cin >> selection;
+                    if (selection == 'Y' || selection == 'y') {
+                        g.generateRandomEdge(allWeights);
+                    }
+                    else {
+                        break;
+                    }
+                }
+                g.toString();
+                cin.get();
+                cin.get();
+                
+            }
+            else if (choice == 3) {
+                while (!g.DFS()) {
+                    char selection;
+                    cout << "The graph does not contain cycles " << endl;
+                    cout << "Would you like to make it to have cycles?" << endl;
+                    cin >> selection;
+                    if (selection == 'Y' || selection == 'y') {
+                        g.generateRandomEdge(allWeights);
+                    }
+                    else {
+                        break;
+                    }
+                }
+                cin.get();
+                cin.get();
+            }
+            else if (choice == 4) {
+                int origin, destination;
+                cout << "Please enter your departure point" << endl;
+                cin >> origin;
+                cout << "Please enter your arrival point" << endl;
+                cin >> destination;
+            }
+        }
+    } while (choice != 8);
    
 }
 
@@ -61,4 +109,36 @@ void initializeWeights(Graph &all, int vertices) {
             weightFile.ignore();
         }
     }
+}
+
+void MainMenu() {
+    system("CLS");
+    cout << "***********************************Graph Traversal********************************************" << endl;
+    cout << "Main Menu: " << endl;
+    cout << "Please select your choice: " << endl;
+    cout << "1. Display graph" << endl;
+    cout << "2: Check Strongly Connected" << endl;
+    cout << "3: Check Cycle Detection" << endl;
+    cout << "4: Find Shortest Path" << endl;
+    cout << "5: Reset Graph" << endl;
+    cout << "6: Add Edge" << endl;
+    cout << "7: Remove Edge" << endl;
+    cout << "8: Exit" << endl;
+    cout << "Your choice: ";
+
+}
+
+bool checkStrong(Graph& g, int V) {
+    Graph inverse(V);
+
+    if (g.isSC()) {
+        inverse.getTranspose(g);
+        if (inverse.isSC()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    return false;
 }
